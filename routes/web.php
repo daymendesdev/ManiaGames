9373ebe1e598c12f47e778f5bb2b508c4a144285
Dayanne Mendes <daymendesdev@gmail.com>
2025-04-29 18:02:03 -0300

    inclusao da page avaliações, sua controller e rota

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Avaliacoes\AvaliacoesController;
 
 Route::get('/', [HomeController::class, 'index']);
+Route::get('/avaliacoes', [AvaliacoesController::class, 'index'])->name('avaliacoes');
+
+
